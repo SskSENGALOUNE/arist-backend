464939ec802b379428002546c1c682f2d64bebd3
SskSENGALOUNE <118923975+SskSENGALOUNE@users.noreply.github.com>
2026-05-30 20:50:04 +0700

    chore: update default access token expiration to 1d in auth config

--- src/config/auth.config.ts
+++ src/config/auth.config.ts
@@ -1,11 +1,11 @@
-import { registerAs } from '@nestjs/config';
+import { registerAs } from "@nestjs/config";
 
 export type AuthConfig = ReturnType<typeof authConfig>;
 
-export const authConfig = registerAs('auth', () => ({
+export const authConfig = registerAs("auth", () => ({
   accessTokenSecret: process.env.JWT_ACCESS_SECRET as string,
-  accessTokenExpiresIn: process.env.JWT_ACCESS_EXPIRES_IN ?? '15m',
+  accessTokenExpiresIn: process.env.JWT_ACCESS_EXPIRES_IN ?? "1d",
   refreshTokenSecret: process.env.JWT_REFRESH_SECRET as string,
-  refreshTokenExpiresIn: process.env.JWT_REFRESH_EXPIRES_IN ?? '7d',
-  bcryptSaltRounds: parseInt(process.env.BCRYPT_SALT_ROUNDS ?? '10', 10),
+  refreshTokenExpiresIn: process.env.JWT_REFRESH_EXPIRES_IN ?? "7d",
+  bcryptSaltRounds: parseInt(process.env.BCRYPT_SALT_ROUNDS ?? "10", 10),
 }));
